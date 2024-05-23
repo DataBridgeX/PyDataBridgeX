1e60e676ae69e7fe2be3216ba7b52a76a6c3c6e4
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-05-23 16:27:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION = "Bridging Innovation, Connecting Data"
 LONG_DESCRIPTION = """PyDataBridge is your go-to solution for database connectivity in Python. Whether you're working with Firebase or other databases, PyDataBridge offers a versatile and intuitive package for seamless integration. Simplify your backend development and unleash the power of Python with PyDataBridge."""
 
